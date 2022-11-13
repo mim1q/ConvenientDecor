e86a368bce891ef646b409f63accb9382813a5b0
mim1q <mim1q.dev@gmail.com>
2022-11-13 12:00:00 +0100

    Updated pitchfork and spade (+ 1 additional variant)

--- src/main/java/com/github/mim1q/seasonalspice/block/PitchforkBlock.java
+++ src/main/java/com/github/mim1q/seasonalspice/block/PitchforkBlock.java
@@ -6,11 +6,12 @@ import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
@@ -21,18 +22,20 @@ import org.jetbrains.annotations.Nullable;
 
 public class PitchforkBlock extends Block {
   public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
-  public static final BooleanProperty TILTED = BooleanProperty.of("tilted");
+  public static final EnumProperty<Type> TYPE = EnumProperty.of("type", Type.class);
   public static final VoxelShape SHAPE = createCuboidShape(4, 0, 4, 12, 8, 12);
 
   public PitchforkBlock(Settings settings) {
-    super(settings.nonOpaque());
-    this.setDefaultState(this.getDefaultState().with(TILTED, false));
+    super(settings.nonOpaque().offsetType(
+      (state) -> state.get(TYPE) == Type.TILTED ? OffsetType.NONE : OffsetType.XZ
+    ));
+    this.setDefaultState(this.getDefaultState().with(TYPE, Type.LYING));
   }
 
   @Override
   protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
     super.appendProperties(builder);
-    builder.add(FACING, TILTED);
+    builder.add(FACING, TYPE);
   }
 
   @Nullable
@@ -45,7 +48,7 @@ public class PitchforkBlock extends Block {
   @SuppressWarnings("deprecation")
   public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
     if (player.isSneaking()) {
-      world.setBlockState(pos, state.cycle(TILTED));
+      world.setBlockState(pos, state.cycle(TYPE));
       return ActionResult.SUCCESS;
     }
     return ActionResult.PASS;
@@ -61,5 +64,16 @@ public class PitchforkBlock extends Block {
   @SuppressWarnings("deprecation")
   public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
     return SHAPE;
+  }
+
+  public enum Type implements StringIdentifiable {
+    STRAIGHT,
+    TILTED,
+    LYING;
+
+    @Override
+    public String asString() {
+      return name().toLowerCase();
+    }
   }
 }
