6b164c7a4cc7b302e92ed7af9138db4958b92628
mim1q <mim1q.dev@gmail.com>
2022-11-17 19:48:19 +0100

    Fixed colors in gui

--- src/main/java/com/github/mim1q/seasonalspice/client/colors/FallLeavesColors.java
+++ src/main/java/com/github/mim1q/seasonalspice/client/colors/FallLeavesColors.java
@@ -9,23 +9,23 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 
 public class FallLeavesColors {
-  public static final int YELLOW = 0x00AAFF;
-  public static final int ORANGE = 0x00AAFF;
-  public static final int RED = 0x00AAFF;
-  public static final int BROWN = 0x00AAFF;
+  public static final int YELLOW = 0xFFF52d;
+  public static final int ORANGE = 0xED7B19;
+  public static final int RED = 0xD12C1D;
+  public static final int BROWN = 0x733420;
 
   public static int getColor(BlockState state, BlockRenderView world, BlockPos pos, int tintIndex) {
     if (state.isOf(SeasonalSpiceBlocks.YELLOW_LEAF_PILE)) {
-      return 0xfff52d;
+      return YELLOW;
     }
     if (state.isOf(SeasonalSpiceBlocks.ORANGE_LEAF_PILE)) {
-      return 0xed7b19;
+      return ORANGE;
     }
     if (state.isOf(SeasonalSpiceBlocks.RED_LEAF_PILE)) {
-      return 0xd12c1d;
+      return RED;
     }
     if (state.isOf(SeasonalSpiceBlocks.BROWN_LEAF_PILE)) {
-      return 0x733420;
+      return BROWN;
     }
     return world.getColor(pos, BiomeColors.FOLIAGE_COLOR);
   }
