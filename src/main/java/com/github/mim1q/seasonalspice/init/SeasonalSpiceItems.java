62c45f73d3b9a7a8115bed0019591990df9f58a1
mim1q <mim1q.dev@gmail.com>
2023-01-06 20:22:38 +0100

    Watering Can is now equippable in the head slot

--- src/main/java/com/github/mim1q/seasonalspice/init/SeasonalSpiceItems.java
+++ src/main/java/com/github/mim1q/seasonalspice/init/SeasonalSpiceItems.java
@@ -3,14 +3,15 @@ package com.github.mim1q.seasonalspice.init;
 import com.github.mim1q.seasonalspice.SeasonalSpice;
 import com.github.mim1q.seasonalspice.item.WateringCanItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.util.registry.Registry;
 
 public class SeasonalSpiceItems {
-
-  public static final WateringCanItem WATERING_CAN = register(new WateringCanItem(
-    new FabricItemSettings().maxCount(1)
-  ), "watering_can");
+  public static final WateringCanItem WATERING_CAN = register(
+    new WateringCanItem(new FabricItemSettings().maxCount(1).equipmentSlot(stack -> EquipmentSlot.HEAD)),
+    "watering_can"
+  );
 
   public static void init() { }
 
