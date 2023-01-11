d123fc46090e268027badeef753d40964dc37005
mim1q <mim1q.dev@gmail.com>
2023-01-11 19:29:52 +0100

    Using custom model when the farmland is hydrated

--- src/main/java/com/github/mim1q/convenientdecor/mixin/block/FarmlandBlockMixin.java
+++ src/main/java/com/github/mim1q/convenientdecor/mixin/block/FarmlandBlockMixin.java
@@ -4,11 +4,9 @@ import com.github.mim1q.convenientdecor.block.CustomProperties;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FarmlandBlock;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -34,11 +32,6 @@ public abstract class FarmlandBlockMixin extends Block {
   @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
   private void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
     if (state.get(CustomProperties.HYDRATED)) {
-      if (world.getBlockState(pos.up()).isAir()) {
-        Vec3d offset = new Vec3d(random.nextDouble() - 0.5D, 0.0D, random.nextDouble() - 0.5D);
-        Vec3d particlePos = Vec3d.ofBottomCenter(pos.up()).add(0.0D, 0.1D, 0.0D);
-        world.spawnParticles(ParticleTypes.SPLASH, particlePos.x, particlePos.y, particlePos.z, 10, offset.x, 0.0D, offset.z, 0.1D);
-      }
       ci.cancel();
     }
   }
