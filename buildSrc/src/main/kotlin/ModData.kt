71568f521a4165ce9cbf63bedc8533b28295a87d
mim1q <mim1q.dev@gmail.com>
2023-01-12 16:50:22 +0100

    0.0.3

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,7 +1,7 @@
 object ModData {
   const val group = "com.github.mim1q"
   const val id = "convenientdecor"
-  const val version = "0.0.2"
+  const val version = "0.0.3"
   const val versionType = "release"
   val mcVersions = listOf("1.19.2")
   val dependencies = listOf("fabric-api")
