1ac86859e5d9f0a0fde872c525718da700a114ee
mim1q <mim1q.dev@gmail.com>
2023-01-21 18:04:36 +0100

    Umbrellas attract lightning

--- src/main/java/com/github/mim1q/convenientdecor/mixin/entity/LivingEntityMixin.java
+++ src/main/java/com/github/mim1q/convenientdecor/mixin/entity/LivingEntityMixin.java
@@ -4,24 +4,33 @@ import com.github.mim1q.convenientdecor.init.ModItems;
 import com.github.mim1q.convenientdecor.item.UmbrellaItem;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Hand;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
   @Shadow public abstract ItemStack getStackInHand(Hand hand);
 
+  @Shadow public abstract void setStackInHand(Hand hand, ItemStack stack);
+
   public LivingEntityMixin(EntityType<?> type, World world) {
     super(type, world);
   }
+
+  private int umbrellaTimer = 0;
 
   private boolean hasUmbrellaInHand(Hand hand, boolean canBeBroken, boolean canBeFolded) {
     ItemStack stack = this.getStackInHand(hand);
@@ -34,6 +43,46 @@ public abstract class LivingEntityMixin extends Entity {
   private boolean holdsUmbrella(boolean canBeBroken, boolean canBeFolded) {
     return hasUmbrellaInHand(Hand.MAIN_HAND, canBeBroken, canBeFolded)
       || hasUmbrellaInHand(Hand.OFF_HAND, canBeBroken, canBeFolded);
+  }
+
+  private void tryBurnUmbrella(Hand hand) {
+    if (hasUmbrellaInHand(hand, false, false)) {
+      this.setStackInHand(hand, ModItems.BROKEN_UMBRELLA.getDefaultStack());
+    }
+  }
+
+  private boolean onlyAirAbove() {
+    for (int i = this.getBlockY(); i <= world.getTopY(); i++) {
+      if (world.isAir(this.getBlockPos().withY(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Inject(method = "tick", at = @At("HEAD"))
+  private void tick(CallbackInfo ci) {
+    if (!this.world.isClient) {
+      if (holdsUmbrella(true, false)
+        && world.isThundering()
+        && world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, this.getBlockPos()).getY() <= this.getY()
+        && onlyAirAbove()
+      ) {
+        umbrellaTimer++;
+      } else {
+        umbrellaTimer = Math.min(0, umbrellaTimer + 1);
+      }
+      if (umbrellaTimer > 20 * 4 && this.random.nextFloat() < 0.5F) {
+        umbrellaTimer = -(20 * 60) - this.random.nextInt(20 * 120);
+        LightningEntity lightning = EntityType.LIGHTNING_BOLT.create(this.world);
+        if (lightning != null) {
+          lightning.setPosition(this.getPos());
+          this.world.spawnEntity(lightning);
+          tryBurnUmbrella(Hand.MAIN_HAND);
+          tryBurnUmbrella(Hand.OFF_HAND);
+        }
+      }
+    }
   }
 
   @ModifyVariable(method = "damage", at = @At("HEAD"), ordinal = 0, argsOnly = true)
@@ -50,5 +99,15 @@ public abstract class LivingEntityMixin extends Entity {
       return Math.min(d, 0.025F);
     }
     return d;
+  }
+
+  @Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
+  private void readCustomDataFromNbt(NbtCompound nbt, CallbackInfo ci) {
+    umbrellaTimer = nbt.getInt("umbrellaTimer");
+  }
+
+  @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
+  private void writeCustomDataToNbt(NbtCompound nbt, CallbackInfo ci) {
+    nbt.putInt("umbrellaTimer", umbrellaTimer);
   }
 }
