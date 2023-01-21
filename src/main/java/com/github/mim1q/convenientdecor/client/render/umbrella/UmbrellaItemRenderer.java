51a2c4084e59b96125b534780459e91c508ab594
mim1q <mim1q.dev@gmail.com>
2023-01-21 16:28:43 +0100

    Figured out angles for all modes

--- src/main/java/com/github/mim1q/convenientdecor/client/render/umbrella/UmbrellaItemRenderer.java
+++ src/main/java/com/github/mim1q/convenientdecor/client/render/umbrella/UmbrellaItemRenderer.java
@@ -21,16 +21,44 @@ public class UmbrellaItemRenderer implements DynamicItemRenderer {
   @Override
   public void render(ItemStack stack, Mode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
     UmbrellaItem item = (UmbrellaItem) stack.getItem();
-    boolean folded = true;
+    boolean folded = false;
     boolean broken = item == ModItems.BROKEN_UMBRELLA;
-    switch (mode) {
-      case GUI -> {
-        float scale = folded ? 0.66F : 0.5F;
-        matrices.scale(scale, scale, scale);
-        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-30.0F));
-        matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(30.0F));
-        matrices.translate(folded ? 1.1F : 1.6F, folded ? 0.0F : 0.15F, 0.0F);
+    if (mode == Mode.GUI) {
+      float scale = folded ? 0.66F : 0.5F;
+      matrices.scale(scale, scale, scale);
+      matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-30.0F));
+      matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(30.0F));
+      matrices.translate(folded ? 1.1F : 1.6F, folded ? 0.0F : 0.15F, 0.0F);
+    }
+    if (mode == Mode.FIRST_PERSON_RIGHT_HAND || mode == Mode.FIRST_PERSON_LEFT_HAND) {
+      float scale = folded ? 1.25F : 1.5F;
+      matrices.scale(scale, scale, scale);
+      matrices.translate(
+        mode == Mode.FIRST_PERSON_RIGHT_HAND ? 0.8F : -0.13F,
+        folded ? -0.5F : -0.1F,
+        folded ? -0.1F : 0.1F
+      );
+      matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
+    }
+    if (mode == Mode.GROUND) {
+      matrices.translate(0.5F, 0.25F, 0.5F);
+      matrices.scale(0.25F, 0.25F, 0.25F);
+    }
+    if (mode == Mode.THIRD_PERSON_LEFT_HAND || mode == Mode.THIRD_PERSON_RIGHT_HAND) {
+      if (folded) {
+        matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(mode == Mode.THIRD_PERSON_LEFT_HAND ? 10.0F : -10.0F));
+        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
+        matrices.translate(-0.5F, 0.2F, -0.7F);
+      } else {
+        matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(70.0F));
+        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(210.0F));
+        matrices.translate(-0.6F, 0.55F, 0.0F);
       }
+    }
+    if (mode == Mode.FIXED) {
+      matrices.translate(0.45F, 0.0F, 0.0F);
+      matrices.scale(0.33F, 0.33F, 0.33F);
+      matrices.translate(0.0F, 0.66F, 1.7F);
     }
     umbrellaRenderer.render(matrices, vertexConsumers, light, overlay, item.color, broken, folded);
   }
