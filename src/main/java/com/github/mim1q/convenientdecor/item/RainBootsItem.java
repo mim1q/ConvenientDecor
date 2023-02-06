67b0c6c55c62359c073c627edc73c0a8fe4a9c1c
mim1q <mim1q.dev@gmail.com>
2023-02-06 21:34:27 +0100

    Rain boots apply speed

--- src/main/java/com/github/mim1q/convenientdecor/item/RainBootsItem.java
+++ src/main/java/com/github/mim1q/convenientdecor/item/RainBootsItem.java
@@ -1,9 +1,15 @@
 package com.github.mim1q.convenientdecor.item;
 
 import com.github.mim1q.convenientdecor.init.ModItems;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.DyeColor;
+import net.minecraft.world.World;
 
 public class RainBootsItem extends ArmorItem implements ColoredItem {
   private final DyeColor color;
@@ -11,6 +17,23 @@ public class RainBootsItem extends ArmorItem implements ColoredItem {
   public RainBootsItem(Settings settings, DyeColor color) {
     super(ModItems.CUSTOM_CLOTHES_ARMOR_MATERIAL, EquipmentSlot.FEET, settings);
     this.color = color;
+  }
+
+  @Override
+  public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+    if (!world.isClient
+      && slot == EquipmentSlot.FEET.getEntitySlotId()
+      && entity instanceof LivingEntity living
+      && world.isRaining()
+    ) {
+      living.addStatusEffect(new StatusEffectInstance(
+        StatusEffects.SPEED,
+        22,
+        world.isThundering() ? 1 : 0,
+        false, false, true
+      ));
+    }
+    super.inventoryTick(stack, world, entity, slot, selected);
   }
 
   @Override
