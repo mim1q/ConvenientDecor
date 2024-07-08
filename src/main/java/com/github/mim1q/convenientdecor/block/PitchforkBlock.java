1211f0fb7d09df6330e66de10a17909c0c8b5c82
mim1q <mim1q.dev@gmail.com>
2024-07-08 18:22:16 +0200

    Fixed the mod crashing when the system language is Turkish

--- src/main/java/com/github/mim1q/convenientdecor/block/PitchforkBlock.java
+++ src/main/java/com/github/mim1q/convenientdecor/block/PitchforkBlock.java
@@ -58,13 +58,19 @@ public class PitchforkBlock extends Block {
   }
 
   public enum Type implements StringIdentifiable {
-    STRAIGHT,
-    TILTED,
-    LYING;
+    STRAIGHT("straight"),
+    TILTED("tilted"),
+    LYING("lying");
+
+    private final String name;
+
+    Type(String name) {
+      this.name = name;
+    }
 
     @Override
     public String asString() {
-      return name().toLowerCase();
+      return name;
     }
   }
 }
